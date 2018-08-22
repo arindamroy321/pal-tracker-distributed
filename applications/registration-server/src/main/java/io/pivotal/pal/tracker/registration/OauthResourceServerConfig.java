59507ddb951c8fb6b2af26c32ee573a7b51e6219
pal_user <arindam.roy@nttdata.com>
2018-08-22 08:53:55 -0600

    registration server code changes new

--- applications/registration-server/src/main/java/io/pivotal/pal/tracker/registration/OauthResourceServerConfig.java
+++ applications/registration-server/src/main/java/io/pivotal/pal/tracker/registration/OauthResourceServerConfig.java
@@ -1,10 +1,16 @@
 package io.pivotal.pal.tracker.registration;
 
 import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
+import org.springframework.cloud.client.loadbalancer.LoadBalanced;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.oauth2.client.OAuth2ClientContext;
+import org.springframework.security.oauth2.client.OAuth2RestTemplate;
+import org.springframework.security.oauth2.client.resource.OAuth2ProtectedResourceDetails;
 import org.springframework.security.oauth2.config.annotation.web.configuration.ResourceServerConfigurerAdapter;
 import org.springframework.security.oauth2.config.annotation.web.configurers.ResourceServerSecurityConfigurer;
+import org.springframework.web.client.RestOperations;
 
 @Configuration
 @ConditionalOnProperty(value = "application.oauth-enabled", matchIfMissing = true)
@@ -20,6 +26,12 @@ public class OauthResourceServerConfig extends ResourceServerConfigurerAdapter {
     public void configure(ResourceServerSecurityConfigurer resources) throws Exception {
         // do not require a resource id in AccessToken.
         resources.resourceId(null);
+    }
+
+    @Bean
+    @LoadBalanced
+    public RestOperations restTemplate(OAuth2ProtectedResourceDetails resource, OAuth2ClientContext oauth2ClientContext) {
+        return new OAuth2RestTemplate(resource, oauth2ClientContext);
     }
 
 
